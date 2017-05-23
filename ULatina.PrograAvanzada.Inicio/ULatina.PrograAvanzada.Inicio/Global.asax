cd44da858f554c2c0f2dda9d72a2cc10b9a0265e
Sebasgz96 <sebasg_z96@outlook.com>
2017-05-22 19:51:09 -0600

    Proyecto con aplicación web base

---
+++ ULatina.PrograAvanzada.Inicio/ULatina.PrograAvanzada.Inicio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ULatina.PrograAvanzada.Inicio.MvcApplication" Language="C#" %>
---
+++ ULatina.PrograAvanzada.Inicio/ULatina.PrograAvanzada.Inicio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ULatina.PrograAvanzada.Inicio.Startup))]
+namespace ULatina.PrograAvanzada.Inicio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
