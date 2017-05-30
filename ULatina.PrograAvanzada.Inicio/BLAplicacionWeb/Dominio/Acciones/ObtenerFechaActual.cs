9dc906388811ff637d7ee2f49dd0f896cad6fd38
sebas <sebas@DESKTOP-SJMVA7O>
2017-05-29 20:20:59 -0600

    Se agrega proyecto de logica de negocios

---
+++ ULatina.PrograAvanzada.Inicio/BLAplicacionWeb/Dominio/Acciones/ObtenerFechaActual.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLAplicacionWeb.Dominio.Acciones
+{
+    public class ObtenerFechaActual
+    {
+        public string ObtengaLaFecha()
+        {
+
+            var laEspecifiacion = new Especificaciones.ObtengaFecha();
+            var laFecha = laEspecifiacion.FormatearUnaFecha(DateTime.Now, "dd/mm/yyyy");
+            return laFecha;
+        }
+    }
+}
